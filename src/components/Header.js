e46cf95c3241d639e6ac1eda6fd7faf32cf2be4a
Julian Pagtama <jpagtama@gmail.com>
2022-05-12 09:52:55 -0700

    Adds top right nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import styles from './Header.module.css'
 const Header = () => {
     return (
         <div className={styles.header} >
-            <h1>Header</h1>
+            <ul className={styles.topRightNav} >
+                <li><a href="#" >Log in</a></li>
+                <li><a href="#" >About</a></li>
+            </ul>
         </div>
     )
 }
