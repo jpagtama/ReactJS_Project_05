151826abf921291218b3b77c7c6f8a0acef9ed7b
Julian Pagtama <jpagtama@gmail.com>
2022-05-12 10:25:39 -0700

    Sets top nav aligned to the top

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,17 @@ import styles from './Header.module.css'
 const Header = () => {
     return (
         <div className={styles.header} >
-            <ul className={styles.topRightNav} >
-                <li><a href="#" >Log in</a></li>
-                <li><a href="#" >About</a></li>
-            </ul>
+            <div className={styles.topNav} >
+                <ul className={styles.topLeftNav} >
+                    <li><a href="#" >Contact us</a></li>
+                    <li><a href="#" >About</a></li>
+                </ul>
+
+                <ul className={styles.topRightNav} >
+                    <li><a href="#" >Sign up</a></li>
+                    <li><a href="#" >Log in</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
