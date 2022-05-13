26285230fca711d7060165d4993800e9a6d45749
Julian Pagtama <jpagtama@gmail.com>
2022-05-13 16:19:07 -0700

    Adds placeholder lorem ipsum

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import styles from './Header.module.css'
 
 const Header = () => {
+
     return (
         <div className={styles.header} >
             <div className={styles.topNav} >
                 <ul className={styles.topLeftNav} >
-                    <li><a href="#" >Contact us</a></li>
-                    <li><a href="#" >About</a></li>
+                    <li><a href="0" >Contact us</a></li>
+                    <li><a href="0" >About</a></li>
                 </ul>
 
                 <ul className={styles.topRightNav} >
-                    <li><a href="#" >Sign up</a></li>
-                    <li><a href="#" >Log in</a></li>
+                    <li><a href="0" >Sign up</a></li>
+                    <li><a href="0" >Log in</a></li>
                 </ul>
             </div>
         </div>
