d247523882af76cba05062c19439b34a3df06ee4
Julian Pagtama <jpagtama@gmail.com>
2022-05-13 16:47:04 -0700

    Adds styling to footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer} >
-            <div>
+            <div className={styles.contentBlock} >
                 <h4>Company</h4>
                 <ul>
                     <li><a href="0" >About Us</a></li>
@@ -13,7 +13,7 @@ const Footer = () => {
                     <li><a href="0" >Privacy Policy</a></li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.contentBlock}>
                 <h4>Your Profile</h4>
                 <ul>
                     <li><a href="0" >My Account</a></li>
