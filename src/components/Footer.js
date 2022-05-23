546b6b996976f39bcc1fd0f0c54440346cec02e0
Julian Pagtama <jpagtama@gmail.com>
2022-05-23 10:36:43 -0700

    Centers footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,25 @@ import styles from './Footer.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer} >
-            <div className={styles.contentBlock} >
-                <h4>Company</h4>
-                <ul>
-                    <li><a href="0" >About Us</a></li>
-                    <li><a href="0" >Careers</a></li>
-                    <li><a href="0" >Support</a></li>
-                    <li><a href="0" >Terms of Service</a></li>
-                    <li><a href="0" >Privacy Policy</a></li>
-                </ul>
-            </div>
-            <div className={styles.contentBlock}>
-                <h4>Your Profile</h4>
-                <ul>
-                    <li><a href="0" >My Account</a></li>
-                    <li><a href="0" >Settings</a></li>
-                    <li><a href="0" >My Cart</a></li>
-                </ul>
+            <div className={styles.container}>
+                <div className={styles.contentBlock} >
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="0" >About Us</a></li>
+                        <li><a href="0" >Careers</a></li>
+                        <li><a href="0" >Support</a></li>
+                        <li><a href="0" >Terms of Service</a></li>
+                        <li><a href="0" >Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div className={styles.contentBlock}>
+                    <h4>Your Profile</h4>
+                    <ul>
+                        <li><a href="0" >My Account</a></li>
+                        <li><a href="0" >Settings</a></li>
+                        <li><a href="0" >My Cart</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
