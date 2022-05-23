4b5e6ce93c6d96d650a7c7edc76bafecc9a7c734
Julian Pagtama <jpagtama@gmail.com>
2022-05-23 10:36:22 -0700

    Adds React Fragment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <>
+    <React.Fragment>
       <Header />
       <Main />
       <Footer />
-    </>
+    </React.Fragment>
   )
 }
 
