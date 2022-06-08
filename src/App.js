ab52c856010ea736e171b631a5568d621f549e04
Julian Pagtama <jpagtama@gmail.com>
2022-06-07 17:01:55 -0700

    Refactors components into layouts folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './components/layouts/Header'
+import Main from './components/layouts/Main'
+import Footer from './components/layouts/Footer'
 
 const App = () => {
   return (
