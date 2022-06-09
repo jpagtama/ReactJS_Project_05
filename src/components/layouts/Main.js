3b4c449cd4acaad6b97cb7c12870092bb67b2dd5
Julian Pagtama <jpagtama@gmail.com>
2022-06-08 20:01:13 -0700

    Removes placeholder text in Main

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -3,18 +3,6 @@ import styles from './Main.module.css'
 const Main = () => {
     return (
         <div className={styles.body} >
-            
-            
-{/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper consequat pulvinar. Ut eget tincidunt ipsum, non efficitur diam. Vestibulum pretium porttitor eleifend. Morbi magna lorem, laoreet hendrerit tellus at, elementum porta nulla. Nam tellus odio, malesuada vel iaculis ut, pulvinar porttitor diam. Nullam ac mi viverra, facilisis massa eget, accumsan metus. Praesent bibendum fringilla nisl, eu venenatis ipsum hendrerit id. Suspendisse potenti. Donec et elit sit amet lacus convallis dapibus. Nunc rutrum nisl vel diam tincidunt aliquam. Aliquam vitae nibh sodales, aliquam eros eu, dapibus diam. In vehicula, augue ut varius auctor, tellus ligula consequat magna, et rutrum eros ante eget est. Sed quis vestibulum nibh.
-
-Donec vel massa varius, vulputate dolor quis, convallis nisi. Integer rutrum placerat risus, ac gravida est tincidunt sit amet. Quisque sit amet nulla risus. Curabitur sollicitudin tempor velit ac scelerisque. Sed a mauris enim. In augue erat, lacinia vel tellus ut, tempor bibendum urna. Nam vehicula auctor ipsum, nec posuere velit auctor ac. Ut posuere leo elit, id sagittis tellus luctus id. Proin eu ante in tellus bibendum rutrum sit amet sit amet ipsum. Nulla nec facilisis sem.
-
-Pellentesque dolor diam, semper sit amet porta sed, ornare et lectus. Duis at diam at enim molestie bibendum aliquam vitae elit. Etiam sagittis pharetra dictum. Fusce id ipsum vel nulla pulvinar commodo. Ut sollicitudin metus in felis hendrerit, sed faucibus arcu aliquam. Morbi eu tortor odio. Vestibulum nec consequat ligula. Maecenas nec ultricies massa. Nullam in nulla facilisis, mollis neque at, tincidunt justo.
-
-Vestibulum tincidunt eleifend sem. Vestibulum iaculis finibus eros. Nunc iaculis nisi a nulla gravida, auctor dictum leo pulvinar. Donec venenatis lobortis dignissim. Cras ultrices quis eros nec mollis. Sed vitae erat id nulla dignissim dapibus ac vitae sem. Duis vitae posuere ante. Vivamus sagittis, tellus id posuere vehicula, enim leo ullamcorper quam, id auctor enim ligula nec metus. Phasellus venenatis nulla sit amet lectus eleifend, at auctor arcu vestibulum. Vestibulum mi nulla, congue sed viverra eu, bibendum at ante. Sed viverra, est sit amet viverra vehicula, ante sem vulputate diam, sit amet feugiat orci nibh non urna. Donec pretium justo neque, at varius eros fermentum ac. Integer eget leo ac quam efficitur vestibulum eu at nibh. Aenean bibendum interdum suscipit. Aenean lacinia libero id massa dapibus luctus. Phasellus orci metus, fermentum quis placerat quis, placerat porta mi.
-
-Aliquam non dapibus magna. Nunc elementum convallis venenatis. Vestibulum nec sapien malesuada, vulputate magna quis, euismod risus. Integer id bibendum mauris, non vulputate elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer ac maximus diam. Integer interdum elementum lacus, eget volutpat urna eleifend in. Nam sit amet libero eget eros semper venenatis dapibus at nisi. Donec tincidunt lacus dui, non elementum diam elementum eget. Nulla facilisi. Pellentesque dapibus laoreet ligula, et auctor quam suscipit sed. Vestibulum et molestie ante. Proin sit amet maximus risus.
-</p> */}
         </div>
     )
 }
