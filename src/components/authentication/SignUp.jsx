a73ecc3bbf77158b0be8fb7a5cd505885ea32999
Julian Pagtama <jpagtama@gmail.com>
2022-06-08 19:59:43 -0700

    Adds SignUp and css module

---
+++ src/components/authentication/SignUp.jsx
@@ -0,0 +1,15 @@
+import styles from './SignUp.module.css'
+
+const SignUp = () => {
+    return (
+        <form>
+            <label>First Name</label>
+            <input />
+            <label>Last Name</label>
+            <input />
+            <label>Email</label>
+            <input />
+        </form>
+    )
+}
+export default SignUp
