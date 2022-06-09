929815610d711f65f7ab7d065be45763b5b66d46
Julian Pagtama <jpagtama@gmail.com>
2022-06-09 15:54:18 -0700

    Adds Button and Input components

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import useInput from '../../hooks/use-input'
 import styles from './Input.module.css'
 
-const Input = props => {
+const Input = (props) => {
+    const inputType = props.type == null? "text": props.type
+    const {value,isValid,touched,errorMessage,changeHandler,blurHandler} = useInput(inputType,props.getValueValidity)
+
     return (
         <React.Fragment>
-            <label htmlFor={props.id} >{props.label}</label>
-            <input id={props.id} className={styles.input} type={props.type == null? "text": props.type} />
+            <label htmlFor={props.id} >{props.label}</label> 
+            {touched && !isValid && <span className={styles.error}>{errorMessage()}</span>}
+            <input id={props.id} className={styles.input} type={inputType} value={value} onChange={changeHandler} onBlur={blurHandler} />
         </React.Fragment>
     )
 }
