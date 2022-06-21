f673d2e587f8be9170856ce0a39ccfd515195327
Julian Pagtama <jpagtama@gmail.com>
2022-06-21 08:40:41 -0700

    Creates signUp reducer in authSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -6,6 +6,12 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
+        signUp(state,action) {
+            state.isLoggedIn = true
+            state.firstName = action.payload.firstName
+            state.lastName = action.payload.lastName
+            state.email = action.payload.email
+        },
         logIn(state) {state.isLoggedIn = true},
         logOut(state) {state.isLoggedIn = false}
     }
