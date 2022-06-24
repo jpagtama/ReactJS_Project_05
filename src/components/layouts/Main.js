b1301011b4d48e35a552f2b0b487a91070daca4d
Julian Pagtama <jpagtama@gmail.com>
2022-06-24 16:30:18 -0700

    Modifies Routes and adds Navigate

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -1,7 +1,8 @@
 import SignUp from '../authentication/SignUp'
 import { useSelector } from 'react-redux'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Welcome from '../pages/Welcome'
+import Products from '../pages/Products'
 import styles from './Main.module.css'
 
 
@@ -10,14 +11,18 @@ const Main = () => {
 
     return (
         <div className={styles.body} > 
-            {!isLoggedIn && 
+            {isLoggedIn && 
                 <Routes>
                     <Route path="/" element={<Welcome />} />
+                    <Route path="/products" element={<Products />} />
+                </Routes>
+            }
+            {!isLoggedIn && 
+                <Routes >
+                    <Route path="/" element={<Navigate to="/signup" />} />
                     <Route path="/signup" element={<SignUp />} />
                 </Routes>
-                
             }
-            {isLoggedIn && <h2 style={{paddingTop: "8rem"}}>You are logged in</h2> }
         </div>
     )
 }
