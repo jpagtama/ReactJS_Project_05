8d44facc61f49ed7ded20723ed61ae429cc21577
Julian Pagtama <jpagtama@gmail.com>
2022-06-24 15:53:16 -0700

    Modifies Input with reference and adds focus to useImperativeHandle hook

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,20 +1,22 @@
-import React, { forwardRef, useImperativeHandle } from 'react'
+import React, { useRef, forwardRef, useImperativeHandle } from 'react'
 import useInput from '../../hooks/use-input'
 import styles from './Input.module.css'
 
 const Input = forwardRef((props,ref) => {
     const inputType = props.type == null? "text": props.type
     const {value,isValid,touched,errorMessage,changeHandler,blurHandler} = useInput(inputType,props.getValueValidity)
+    const inputRef = useRef()
 
     useImperativeHandle(ref, () => ({
         value: value,
+        focus: () => inputRef.current.focus()
     }))
 
     return (
         <React.Fragment>
             <label htmlFor={props.id} >{props.label}</label> 
             {touched && !isValid && <span className={styles.error}>{errorMessage()}</span>}
-            <input id={props.id} className={styles.input} type={inputType} value={value} onChange={changeHandler} onBlur={blurHandler} />
+            <input ref={inputRef} id={props.id} className={styles.input} type={inputType} value={value} onChange={changeHandler} onBlur={blurHandler} />
         </React.Fragment>
     )
 })
