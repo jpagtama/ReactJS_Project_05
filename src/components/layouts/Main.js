bfe5427b746e00fb7c8a5c34ea2d3bd8f880f63d
Julian Pagtama <jpagtama@gmail.com>
2022-06-23 17:34:02 -0700

    Adds Routes to Main component

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -1,5 +1,7 @@
 import SignUp from '../authentication/SignUp'
 import { useSelector } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
+import Welcome from '../pages/Welcome'
 import styles from './Main.module.css'
 
 
@@ -7,8 +9,14 @@ const Main = () => {
     const isLoggedIn = useSelector(state => state.user.isLoggedIn)
 
     return (
-        <div className={styles.body} >
-            {!isLoggedIn && <SignUp />}
+        <div className={styles.body} > 
+            {!isLoggedIn && 
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/signup" element={<SignUp />} />
+                </Routes>
+                
+            }
             {isLoggedIn && <h2 style={{paddingTop: "8rem"}}>You are logged in</h2> }
         </div>
     )
