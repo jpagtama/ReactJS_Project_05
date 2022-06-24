2fa75336b9a7a74ea5cc06895bea9fd2460050f9
Julian Pagtama <jpagtama@gmail.com>
2022-06-24 16:30:59 -0700

    Modifies Welcome with button and styling to Products page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../ui/Button'
+import styles from '../../styles/Welcome.module.css'
 
 const Welcome = () => {
+  const navigate = useNavigate()
+  const viewProductsHandler = event => {
+    navigate('/products')
+  }
+
   return (
-    <div>Welcome</div>
+    <div className={styles.container}>
+      <Button type="button" onClick={viewProductsHandler} >View Products</Button>
+    </div>
   )
 }
 
