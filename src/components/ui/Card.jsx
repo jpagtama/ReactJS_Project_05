a47025bb2c91373aee7e821edf02028e442c31de
Julian Pagtama <jpagtama@gmail.com>
2022-06-24 16:53:49 -0700

    Adds Card component to UI folder

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/Card.module.css'
+
+const Card = props => {
+  return (
+    <div className={`${props.className} ${styles.card}`}>{props.children}</div>
+  )
+}
+
+export default Card
