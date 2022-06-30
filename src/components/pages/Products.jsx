c8de9b6fec0c7aae9de593be74d4102c92d8bcc8
Julian Pagtama <jpagtama@gmail.com>
2022-06-29 17:52:32 -0700

    Adds Products component with css module

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Card from '../ui/Card'
+import ProductItem from '../Products/ProductItem'
+import useProducts from '../../hooks/useProducts'
 import styles from '../../styles/Products.module.css'
 
 const Products = () => {
+  const {products, isLoading} = useProducts()
+  const productItems = products.map(item => <ProductItem key={item.id} {...item} />)
   return (
     <Card className={styles.products}>
-      <div>Products</div>
+      <ul className={styles.productsList}>
+        {isLoading && <h1>Loading...</h1>}
+        {productItems}
+      </ul>
     </Card>
   )
 }
